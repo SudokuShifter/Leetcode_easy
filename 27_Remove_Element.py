423127a034a663dfc5d342c8215a48a6aa9b1902
mr.Robot <zanid227@yandex.ru>
2024-04-11 14:50:11 +0300

    delet elem2

--- 27_Remove_Element.py
+++ 27_Remove_Element.py
@@ -1,16 +1,3 @@
-# nums = [3, 2, 2, 3]
-# count = 0
-# tmp = 0
-# for i in range(len(nums)):
-#     if nums[i] != 3:
-#         count += 1
-#     else:
-#         tmp = nums.pop(i)
-#         nums.append(tmp)
-
-# print(help(list))
-
-
 nums = [3, 2, 2, 3]
 val = 3
 
