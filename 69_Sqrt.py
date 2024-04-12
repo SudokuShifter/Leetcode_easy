7653c79a61950a280b3502050c48da5331f34b14
mr.Robot <zanid227@yandex.ru>
2024-04-13 00:44:28 +0300

    add 69 task

---
+++ 69_Sqrt.py
@@ -0,0 +1,17 @@
+x = 101
+
+left = 1
+right = x
+
+
+while right - left > 1:
+    middle = (left + right) // 2 
+    if middle * middle > x:
+        right = middle
+    else:
+        left = middle         
+
+print(left)    
+
+
+
