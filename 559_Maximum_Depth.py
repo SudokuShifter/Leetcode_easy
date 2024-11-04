0c05f6517bd640e5db342030f544c766f67d3962
mr.Robot <zanid227@yandex.ru>
2024-11-04 21:58:22 +0300

    add 559 task

---
+++ 559_Maximum_Depth.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.children:
+            return 1
+        result = max([self.maxDepth(i) for i in root.children])
+        return result + 1
